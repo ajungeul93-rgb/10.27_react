af50377868c3817287258a8376332e3516020b89
백준걸 <ajungeul@naver.com>
2025-10-28 17:15:57 +0900

    10/28

--- my-first-react/src/components/chapter03/Chapter03.jsx
+++ my-first-react/src/components/chapter03/Chapter03.jsx
@@ -1,11 +1,18 @@
 const Chapter03 = () => {
 
+    let count = 0;
+
+    const onClickButton = () {
+        count += 1;
+        console.log(count);
+    };
+
     return (
         <>
             <h1>완전 중요한 친구</h1>
             <br />
-            <h3>0</h3>
-            <button>나 클릭하면 숫자 증가함</button>
+            <h3>{count}</h3>
+            <button onClick={onClickButton}>나 클릭하면 숫자 증가함</button>
         </>
     );
 };
